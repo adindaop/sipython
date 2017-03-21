546d8e39f43be101ca788cb50a53bc86f3fbe275
Adinda <adindaoctadia@gmail.com>
2017-03-21 15:10:08 +0700

    vote and results

--- poll/urls.py
+++ poll/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     # /poll/help_me
     url(r'^help_me/$', views.help_me, name='help_me'),
     # ex: /polls/5/results/
-    url(r'^(?P<question_id>[0-9]+)/$', views.detail_question, name='results'),
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail_question, name='detail_question'),
+    # /poll/vote/
+    url(r'^vote/$', views.vote, name='vote'),
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
 ]
